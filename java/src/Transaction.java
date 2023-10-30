03ad538f82c2f4faf47409d77e3d07e7a5a18685
NeksHinto <nhinojo@itba.edu.ar>
2023-10-30 12:13:51 -0300

    Clase 29/10: TP10 con Java

---
+++ java/src/Transaction.java
@@ -0,0 +1,22 @@
+public class Transaction{
+    private String type;
+    private float amount;
+    private Account destination_account;
+    //null no es valor de default porque tengo doble constructor
+
+    public Transaction(String type, float amount, Account destination_account) {
+        this.type = type;
+        this.amount = amount;
+        if (type == "Transfer")
+            this.destination_account = destination_account;
+    }
+    //múltiples constructores. En el caso de que instancien con Y sin destination account
+    public Transaction(String type, float amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+}
